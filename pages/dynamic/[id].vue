13d956f5859de8a3770bfb8a53b408983e6efea4
Nawinkishore <nawinkishore2004@gmail.com>
2024-02-02 19:43:24 +0530

    feat:dynamicRouting

---
+++ pages/dynamic/[id].vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <div>
+          <p>Student id:{{studentId}}</p>  
+        </div>
+    </div>
+</template>
+<script>
+export default{
+    data(){
+        return{
+            studentId:this.$route.params.id,
+        }
+    }
+}
+</script>
---
+++ pages/dynamic/index.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <div>
+      <h1>Hello</h1>
+      <p v-for="(student,index) in students" :key="index"><NuxtLink :to="`/dynamic/${student.studentId}`">{{student.studentName}}</NuxtLink></p>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      students: [
+        {
+          studentId: 1,
+          studentName: "Nawin",
+        },
+        {
+          studentId: 2,
+          studentName: "Na",
+        },
+        {
+          studentId: 3,
+          studentName: "N",
+        },
+      ],
+    };
+  },
+};
+</script>
